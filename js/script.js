628f50ecfc14c1e16fee65f298aab32d74856ac8
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2023-04-11 19:09:23 +0200

    check password, security tips

--- js/script.js
+++ js/script.js
@@ -5,11 +5,54 @@ var swiper = new Swiper(".mySwiper", {
     // loop: true,
     // loopFillGroupWithBlank: true,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  });
+});
+
+
+function check() {
+    var input = document.getElementById("password").value;
+
+    input = input.trim();
+    document.getElementById("password").value = input;
+    if (input.length >= 5) {
+        document.getElementById("check0").style.color = "green";
+    }
+    else {
+        document.getElementById("check0").style.color = "red";
+    }
+
+    if (input.length <= 15) {
+        document.getElementById("check1").style.color = "green";
+    }
+    else {
+        document.getElementById("check1").style.color = "red";
+    }
+
+    if (input.match(/[0-9]/i)) {
+        document.getElementById("check2").style.color = "green";
+    }
+    else {
+        document.getElementById("check2").style.color = "red";
+    }
+
+    if (input.match(/[^A-Za-z0-9-' ']/i)) {
+        document.getElementById("check3").style.color = "green";
+    }
+    else {
+        document.getElementById("check3").style.color = "red";
+    }
+
+    if (input.match(' ')) {
+        document.getElementById("check4").style.color = "red";
+    }
+    else {
+        document.getElementById("check4").style.color = "green";
+    }
+
+}
